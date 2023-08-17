2d30d84f21a35ad8faad5fc3559720038d116e07
Edward Younskevicius <eyounskevicius@fullsail.com>
2023-08-17 15:48:20 -0700

    Implement Exercise 7: Dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Work_Sans, Spline_Sans_Mono } from 'next/font/google';
 import clsx from 'clsx';
+import { cookies } from 'next/headers';
 
 import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
 
@@ -23,19 +24,19 @@ const monoFont = Spline_Sans_Mono({
 });
 
 function RootLayout({ children }) {
-  // TODO: Dynamic theme depending on user preference
-  const theme = 'light';
+  const savedTheme = cookies().get('color-theme');
+  const initialTheme = savedTheme?.value || 'light';
 
   return (
     <RespectMotionPreferences>
       <html
         lang="en"
         className={clsx(mainFont.variable, monoFont.variable)}
-        data-color-theme={theme}
-        style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+        data-color-theme={initialTheme}
+        style={initialTheme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
       >
         <body>
-          <Header theme={theme} />
+          <Header initialTheme={initialTheme} />
           <main>{children}</main>
           <Footer />
         </body>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,39 @@
+'use client';
+
 import React from 'react';
 import clsx from 'clsx';
 import { Rss, Sun, Moon } from 'react-feather';
+import { LIGHT_COLORS, DARK_COLORS } from '@/constants';
+import Cookie from 'js-cookie';
 
 import Logo from '@/components/Logo';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './Header.module.css';
 
-function Header({ theme, className, ...delegated }) {
+function Header({ initialTheme, className, ...delegated }) {
+  const [theme, setTheme] = React.useState(initialTheme);
+
+  function handleClick() {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+
+    setTheme(nextTheme);
+
+    Cookie.set('color-theme', nextTheme, {
+      expires: 1000,
+    });
+
+    const root = document.documentElement;
+    const colors = nextTheme === 'light' ? LIGHT_COLORS : DARK_COLORS;
+
+    root.setAttribute('data-color-theme', nextTheme);
+    Object.entries(colors).forEach(([key, value]) => {
+      root.style.setProperty(key, value);
+    });
+  }
+
   return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
+    <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
 
       <div className={styles.actions}>
@@ -24,15 +45,11 @@ function Header({ theme, className, ...delegated }) {
               transform: 'translate(2px, -2px)',
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+        <button className={styles.action} onClick={handleClick}>
+          {theme === 'light' ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
